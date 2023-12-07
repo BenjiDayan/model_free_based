b9ce8fb3c1502a174553d0a30f83f8672aab2f31
Benjamin Dayan <benjamin.l.dayan@gmail.com>
2023-12-07 15:11:53 +0100

    test out decent amount with some plots of running a model on some input reward RW data

--- models.py
+++ models.py
@@ -5,6 +5,9 @@ def cbar(choice):
     """Gives the other choic: 0 -> 1, 1 -> 0"""
     return 1 - choice
 
+### NB we initialise most Q(s,a) values with 0.5 as in rewards are given
+# with some probability between 0.25 and 0.75, depending on the random walk
+
 class Qclass:
     def __init__(self, beta=1.0, alpha=0.1):
         self.beta = beta
@@ -20,14 +23,16 @@ class Qstage2(Qclass):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        # Q(st, c2,t)
-        self.Q = {(a, b): 0 for a in [0, 1] for b in [0, 1]} 
+        # Q[s][c]]
+        self.Q = np.array([[0.5, 0.5], [0.5, 0.5]])
 
     def get_val(self, key):
+        # key is a (s, c) tuple
         return self.Q[key]
 
     def update(self, state, choice2, r):
-        # Q(st, c2,t)
+        # We learn a new Q value for the state and choice2. Tis the delta rule
+        # Q_t+1(s, t) = (1-alpha) * Q_t(s, t) + alpha * r
         self.Q[(state, choice2)] = (1 - self.alpha) * self.Q[(state, choice2)] + self.alpha * r
 
 class Q_MB(Qclass):
@@ -40,21 +45,27 @@ class Q_MB(Qclass):
         choice2 = cbar(choice1)
         # Gillan seems to use rare_prob=0.0, as if MB simplifies the situation and just thinks this way
         # DawDayan uses the "proper" rare_prob=0.3 as in the experiment.
+        # Either way, the value of the choice is the max of the resultant Q values. Even if we don't always
+        # choose the max of the two choices at the second stage.
         return (1-self.rare_prob) * max(self.Qstage2.Q[(choice1, 0)], self.Qstage2.Q[(choice1, 1)]) + \
                 self.rare_prob * max(self.Qstage2.Q[(choice2, 0)], self.Qstage2.Q[(choice2, 1)])
 
 
 class Q_MF0(Qclass):
-    def __init__(self, **kwargs):
+    def __init__(self, Qstage2, **kwargs):
         super().__init__(**kwargs)
 
-        # Q(choice1)
-        self.Q = {0: 0, 1: 0}
+        # Q(choice1) where choice1 is in 0, 1
+        self.Q = np.array([0.5, 0.5])
+
+        self.Qstage2 = Qstage2
 
     def get_val(self, choice1):
         return self.Q[choice1]
 
     def update(self, choice1, stage2, choice2):
+        # TD(0) update means we bootstrap from the value at the next stage. This is actually SARSA(0), as updates
+        # to Q follow into changes in the policy.
         self.Q[choice1] = (1 - self.alpha) * self.Q[choice1] + self.alpha * self.Qstage2.Q[(stage2, choice2)]
 
     
@@ -63,12 +74,13 @@ class Q_MF1(Qclass):
         super().__init__(**kwargs)
 
         # Q(choice1)
-        self.Q = {0: 0, 1: 0}
+        self.Q = np.array([0.5, 0.5])
 
     def get_val(self, choice1):
         return self.Q[choice1]
 
     def update(self, choice1, reward):
+        # SARSA(1) means update as montecarlo for the whole episode, which is just the end reward.
         self.Q[choice1] = (1 - self.alpha) * self.Q[choice1] + self.alpha * reward
 
 
@@ -81,7 +93,7 @@ class Model:
 
         self.Qstage2 = Qstage2(beta=beta_stage2, alpha=alpha)
         self.Q_MB = Q_MB(self.Qstage2, beta=beta_mb)
-        self.Q_MF0 = Q_MF0(beta=beta_mf0, alpha=alpha)
+        self.Q_MF0 = Q_MF0(self.Qstage2, beta=beta_mf0, alpha=alpha)
         self.Q_MF1 = Q_MF1(beta=beta_mf1, alpha=alpha)
 
 
@@ -115,7 +127,29 @@ class Model:
         stage2 = np.random.choice([choice1, cbar(choice1)], p=[1-self.rare_prob, self.rare_prob])
         choice2 = self.get_stage2_action(stage2)
         reward_prob = [[drift1, drift2], [drift3, drift4]][stage2][choice2]
+        reward = np.random.choice([0, 1], p=[1-reward_prob, reward_prob])
+        return choice1, stage2, choice2, reward
+    
+    def update(self, choice1, stage2, choice2, reward):
+        self.Qstage2.update(stage2, choice2, reward)
+        self.Q_MF0.update(choice1, stage2, choice2)
+        self.Q_MF1.update(choice1, reward)
 
+    def perform_trials(self, drifts_iterator, save_Qs=False):
+        prev_choice1 = None
+        outs = []
+        for (drift1, drift2, drift3, drift4) in drifts_iterator:
+            choice1, stage2, choice2, reward = self.perform_trial(drift1, drift2, drift3, drift4, prev_choice1=prev_choice1)
+            self.update(choice1, stage2, choice2, reward)
+            prev_choice1 = choice1
+            if save_Qs:
+                outs.append((choice1, stage2, choice2, reward, self.Qstage2.Q.copy(), self.Q_MF0.Q.copy(), self.Q_MF1.Q.copy()))
+            else:
+                outs.append((choice1, stage2, choice2, reward))
+
+        return outs
+
+        
 
 
 # Peter: Start with generating not fitting.
